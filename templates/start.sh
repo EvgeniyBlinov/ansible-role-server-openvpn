6ff657c4750b933b1a4174eb8d6d03f264ff0f27
Blinov Evgeniy <evgeniy_blinov@mail.ru>
2021-04-29 18:58:16 +0300

    master
    Work stage 2021-04-29 18:58:16
     M templates/start.sh

--- templates/start.sh
+++ templates/start.sh
@@ -8,6 +8,7 @@ OVPN_DATA="$ABSOLUTE_PATH/data"
 OVPN_PROTO="${OVPN_PROTO:-udp}"
 OVPN_SERVER_NAME="${OVPN_SERVER_NAME:-{{ openvpn__server_name }}}"
 OVPN_PORT="${OVPN_PORT:-1194}"
+OVPN_IMAGE_VERSION="${OVPN_IMAGE_VERSION:-_2020-12-03}"
 
 mkdir -p $OVPN_DATA
 cd $OVPN_DATA
@@ -29,7 +30,7 @@ function configure {
 }
 
 function start {
-  docker run -v $OVPN_DATA:/etc/openvpn -d -p ${OVPN_PORT}:1194/udp --privileged {{ openvpn__image_name }}
+  docker run -v $OVPN_DATA:/etc/openvpn -d -p ${OVPN_PORT}:1194/udp --privileged {{ openvpn__image_name }}${OVPN_IMAGE_VERSION}
 }
 
 $ACTION
